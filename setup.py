d69afbe4137adcccf6c4e0cb17c0ecc24c75a07b
Aaron VanDerlip <aaronvanderlip@users.noreply.github.com>
2011-04-10 10:39:00 -0400

    Added error handling fixed typo in css handling

---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy==0.5.1',
+    'transaction',
+    'repoze.tm2',
+    'zope.sqlalchemy',
+    'FeedParser',
+    'beaker',
+    'BeautifulSoup',
+    'pyramid_zcml',
+    'Chameleon<1.9999'
+    ]
+
+if sys.version_info[:3] < (2,5,0):
+    requires.append('pysqlite')
+
+setup(name='tleave',
+      version='0.1',
+      description='tleave',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: BFG",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='tleave',
+      install_requires = requires,
+      entry_points = """\
+      [paste.app_factory]
+      app = tleave.run:app
+      """
+      )
+
---
+++ tleave/teastrainer.py
@@ -0,0 +1,75 @@
+from BeautifulSoup import BeautifulSoup
+import urllib2
+
+
+def getSchedule(route='NBRYROCK', direction='O', timing='W'):
+    """returns a mapping of the commuter rail schedule"""
+    #need to add timing=W, timing=S, timing=U
+    url = 'http://mbta.com/schedules_and_maps/rail/lines/?route=%s&direction=%s&timing=%s' % (route, direction,timing)
+    response = urllib2.urlopen(url)
+    html = response.read()
+    soup = BeautifulSoup(html)
+    try:
+        links = soup.table.nextSibling
+        return stations(links)
+    except AttributeError:
+        return  None
+    
+def stations(links):
+    stationtimes = {}
+    #remove the first row
+
+    #first we find the row with the train numbers
+    train_number_row = links.find('tr')
+    #build a list of those train numbers, removing any elements in the row that are empty 
+    train_number_cols = train_number_row.findAll('td')
+    train_numbers = [elem.contents[0] for elem in train_number_cols if elem.first() != None]
+    #remove the row so further processing can continue
+    train_number_row.extract()
+    links = links.findAll('tr')
+    stationorder = 0
+    for row in links:
+        stationlist = row.findAll('td', 'hidden')
+        try:
+             station = stationlist[0].renderContents()
+        #set the train number using stationorder     
+        except IndexError:
+             station = ''
+        if len(stationlist):
+                times = cleanTimeTables(row, train_numbers)
+                stationtimes.setdefault(station,(times,stationorder))
+        stationorder += 1
+    return stationtimes
+
+def cleanTimeTables(row, train_numbers):
+    cleantimes =[]
+    #remove the station
+    remove = row.find('td')
+    remove.extract()
+    row = row.findAll('td')
+    
+    #assume that we are at the 0 column for appending train numbers as they are
+    #at the head of each column
+    col_num = 0
+    #inspect all colums in row for time information
+    for time in row:
+        tt = time.renderContents()
+        if tt != '&nbsp;':
+            tt = tt.replace('<span>F</span>','')
+            tt = tt.replace('<span class="flagstop">VIA</span>','')
+            tt = tt.replace('<span class="flagstop">LOW</span>','')
+            train = {'train_num': train_numbers[col_num], 'time': tt}
+            cleantimes.append(train)
+        if tt == '&nbsp;':
+            train = {'train_num': train_numbers[col_num], 'time': 'NONE'}
+            cleantimes.append(train)
+        col_num += 1    
+    return cleantimes
+        
+def prettyPrint(route='NBRYROCK',direction='O'):
+    stationtimes = getSchedule(route,direction) 
+    for station, times in stationtimes.items():
+        print '\n' + station
+        print '   '.join([time for time in times])
+
+    
