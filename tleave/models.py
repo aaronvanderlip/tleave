dc2e0d0eb016d90f59ccf76a8217b02d0ebf6aa7
Aaron VanDerlip <aaronvanderlip@users.noreply.github.com>
2011-05-01 21:27:38 -0400

    Updated UI to use jquery mobile

---
+++ tleave/models.py
@@ -0,0 +1,86 @@
+import transaction
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import MetaData
+from sqlalchemy import Table
+from sqlalchemy import Unicode
+from sqlalchemy import Text 
+from sqlalchemy import ForeignKey 
+from sqlalchemy import DateTime 
+
+from sqlalchemy.exc import IntegrityError
+
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import mapper
+from sqlalchemy.orm import relation, backref 
+
+from zope.sqlalchemy import ZopeTransactionExtension
+from repoze.lru import lru_cache
+
+
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+
+metadata = MetaData()
+
+class Station (object):
+
+    def __init__(self, stationname, routeorder, direction):
+        self.stationname = stationname
+        self.routeorder = routeorder
+        #need to be fixed when import is fixed
+        self.direction = direction
+
+station_table = Table(        
+        'station',
+        metadata,
+        Column('id',Integer, primary_key=True), 
+        #need station order
+        #need line that it belongs to
+        Column('routeorder',Integer),
+        Column('stationname',Text),
+        #is this even being used
+        Column('route',Text),
+        Column('timing',Text, nullable=True),
+        Column('direction',Text, nullable=True),
+
+        )
+models_mapper = mapper(Station,station_table)
+
+class TimeTable (object):
+
+    def __init__(self, time, train_num):
+        self.time = time
+        self.train_num = train_num 
+
+    @lru_cache(1000)
+    def __repr__(self):
+        """get a default formatted time string"""
+        try:
+            time ='         ' + self.time.strftime('%I:%M %p')
+        except AttributeError:
+            time = '  No Train  '    
+        return time
+
+timetable = Table(
+        'timetable',
+        metadata,
+        Column('id',Integer, primary_key=True), 
+        Column('train_num',Text), 
+        Column('station_id',Integer, ForeignKey('station.id')),
+        Column('time',DateTime, nullable=True, default=None),
+        )
+
+models_mapper = mapper(TimeTable,timetable,properties={    
+    'station':relation(Station, backref='timetable', order_by='id')
+    }    )
+
+def initialize_sql(db_string, echo=False):
+    engine = create_engine(db_string, echo=echo)
+    DBSession.configure(bind=engine)
+    metadata.bind = engine
+    metadata.create_all(engine)
+
---
+++ tleave/views.py
@@ -0,0 +1,51 @@
+from exceptions import Exception
+from webob.exc import HTTPFound
+from pyramid.url import route_url
+from pyramid.view import view_config
+from pyramid.renderers import render_to_response
+from tleave.utils import importAllSchedules, nextTrain, getTiming, determineDirection, get_alerts, FRIENDLYROUTES,FEEDS
+from tleave.models import Station
+from tleave.models import DBSession
+
+TIMINGS = [('Weekday','W'), ('Saturday', 'S'), ('Sunday','U')]
+
+def import_schedule(request):
+    importAllSchedules()
+    return HTTPFound(location = route_url('/', request, pagename='FrontPage'))
+
+def index(request,route='NBRYROCK',stationStart='North Station', stationEnd='Rockport',direction='I',timing='W',feed=11,debug='False'):
+    """Handle the front-page."""    
+
+    try:
+        if 'form.submitted' in request.params:
+            route = request.params['route']
+            stationStart = request.params['stationStart']
+            stationEnd = request.params['stationEnd']
+            timing = request.params['timing']
+            feed = FEEDS[request.params['route']]
+        else:   
+            timing = getTiming()    
+
+        station = DBSession.query(Station).filter(Station.route==route).filter(Station.direction==direction).filter(Station.timing==timing).order_by(Station.routeorder)
+        direction = determineDirection(stationStart,stationEnd,route)
+        nexttrain=nextTrain(stationStart,stationEnd,route,timing, direction)
+        #had to convert FRIENDLYROUTES to a list of tuples, not sure why you can't pass a dict
+        return dict(project='tLeave',stationpages=station,routes=FRIENDLYROUTES.items(),nexttrain=nexttrain, selectedroute=route, stationStart=stationStart, stationEnd=stationEnd,debug=debug, direction=direction, timing=(timing), timings=TIMINGS, alerts=get_alerts(feed))    
+    except Exception, e:
+        return render_to_response('tleave:templates/error.pt', '', request=request)
+
+
+
+@view_config(renderer='json')
+def stationlist(request,route='NBRYROCK',direction='O',sortorder='O'):
+    """Handle the front-page."""
+    route = request.params['route']
+    sortorder = request.params['sortorder']
+
+    stations = DBSession.query(Station).filter(Station.route==route).filter(Station.direction==direction).order_by(Station.routeorder)
+    #gahh do i have to build a string here?  need better ajax widget that can comprehend lists
+    stationlist = [ station.stationname for station in stations]
+    if sortorder == 'O':
+        stationlist.reverse()
+    return stationlist
+
