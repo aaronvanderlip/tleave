3b01531d726b357b58988fdf126fc4d794c524f3
Aaron VanDerlip <Aaron VanDerlip@localhost>
2013-02-10 12:12:42 -0500

    Cleanup

--- tleave/run.py
+++ tleave/run.py
@@ -2,7 +2,7 @@ from pyramid.config import Configurator
 from pyramid.renderers import JSON
 from sqlalchemy import engine_from_config
 
-from tleave.views import stationlist, index, import_schedule
+from tleave.views import stationlist, import_schedule
 from tleave.models import (
     DBSession,
     Base,
@@ -21,12 +21,14 @@ def app(global_config, **settings):
     config = Configurator(settings=settings)
     config.begin()
     config.add_route('home', '/')
-    config.add_view(index, route_name='home')
     config.add_route('import', 'import')
     config.add_view(import_schedule, route_name='import')
+
     config.add_renderer('stationjson', JSON(indent=4))
     config.add_route('stationlist', 'stationlist/')
     config.add_view(stationlist, route_name='stationlist', renderer='stationjson')
+
     config.add_static_view('static', 'templates/static')
+    config.scan()
     config.end()
     return config.make_wsgi_app()
